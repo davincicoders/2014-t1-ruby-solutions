37f6d209401e2231b986ae8a8388e237a64c689c
Daniel Stutzman <dtstutz@gmail.com>
2014-03-27 13:38:00 -0600

    Rename products.price_per_unit to price_per_item.

--- 340-grocery-store/app/models/cart_line.rb
+++ 340-grocery-store/app/models/cart_line.rb
@@ -2,6 +2,6 @@ class CartLine < ActiveRecord::Base
   belongs_to :product
 
   def line_price
-    self.product.price_per_unit * self.quantity
+    self.product.price_per_item * self.quantity
   end
 end
